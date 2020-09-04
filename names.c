e1ac9b383d6970698168d3741ee3da961ebe52ce
eball <eball5@huskers.unl.edu>
2020-09-04 17:04:22 +0000

    added code

---
+++ names.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Ellie Ball eball
+ * Date: 2020/09/04
+ * 
+ * program that prints my name
+ */
+ 
+ #include<stdlib.h>
+ #include<stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("Ellie Ball \n");
+     
+     return(0);
+ }
